aa98adbb082a31eefe3770753e07abc6a1d0cd1c
Yan Zong <yan@yzong.com>
2022-11-02 15:08:47 +0800

    Added the code of computing differences

--- utils/dataTools.py
+++ utils/dataTools.py
@@ -2525,7 +2525,7 @@ class Flocking(_data):
             print("\tComputing the agent states...", end = ' ', flush = True)
         
         # Compute the states
-        stateAll = self.computeStates(posAll, velAll, commGraphAll)
+        stateAll = self.computeStates(posAll, velAll, offsetAll, skewAll, commGraphAll)
         
         self.state = {}
         
@@ -3333,13 +3333,91 @@ class Flocking(_data):
     
     def computeDifferences(self, u):
         
+
+
+
+
+        # Takes as input a tensor of shape
+        #   nSamples x tSamples x 2 x nAgents
+        # or of shape
+        #   nSamples x 2 x nAgents
+        # And returns the elementwise difference u_i - u_j of shape
+        #   nSamples (x tSamples) x 2 x nAgents x nAgents
+        # And the distance squared ||u_i - u_j||^2 of shape
+        #   nSamples (x tSamples) x nAgents x nAgents
+        
+        # Check dimensions
+        assert len(u.shape) == 3 or len(u.shape) == 4
+        # If it has shape 3, which means it's only a single time instant, then
+        # add the extra dimension so we move along assuming we have multiple
+        # time instants
+        if len(u.shape) == 3:
+            u = np.expand_dims(u, 1)
+            hasTimeDim = False
+        else:
+            hasTimeDim = True
+        
+        # Now we have that pos always has shape
+        #   nSamples x tSamples x 2 x nAgents
+        nSamples = u.shape[0]
+        tSamples = u.shape[1]
+        assert u.shape[2] == 2
+        nAgents = u.shape[3]
+        
+        # Compute the difference along each axis. For this, we subtract a
+        # column vector from a row vector. The difference tensor on each
+        # position will have shape nSamples x tSamples x nAgents x nAgents
+        # and then we add the extra dimension to concatenate and obtain a final
+        # tensor of shape nSamples x tSamples x 2 x nAgents x nAgents
+        # First, axis x
+        #   Reshape as column and row vector, respectively
+        uCol_x = u[:,:,0,:].reshape((nSamples, tSamples, nAgents, 1))
+        uRow_x = u[:,:,0,:].reshape((nSamples, tSamples, 1, nAgents))
+        #   Subtract them
+        uDiff_x = uCol_x - uRow_x # nSamples x tSamples x nAgents x nAgents
+        # Second, for axis y
+        uCol_y = u[:,:,1,:].reshape((nSamples, tSamples, nAgents, 1))
+        uRow_y = u[:,:,1,:].reshape((nSamples, tSamples, 1, nAgents))
+        uDiff_y = uCol_y - uRow_y # nSamples x tSamples x nAgents x nAgents
+        # Third, compute the distance tensor of shape
+        #   nSamples x tSamples x nAgents x nAgents
+        uDistSq = uDiff_x ** 2 + uDiff_y ** 2
+        # Finally, concatenate to obtain the tensor of differences
+        #   Add the extra dimension in the position
+        uDiff_x = np.expand_dims(uDiff_x, 2)
+        uDiff_y = np.expand_dims(uDiff_y, 2)
+        #   And concatenate them
+        uDiff = np.concatenate((uDiff_x, uDiff_y), 2)
+        #   nSamples x tSamples x 2 x nAgents x nAgents
+            
+        # Get rid of the time dimension if we don't need it
+        if not hasTimeDim:
+            # (This fails if tSamples > 1)
+            uDistSq = uDistSq.squeeze(1)
+            #   nSamples x nAgents x nAgents
+            uDiff = uDiff.squeeze(1)
+            #   nSamples x 2 x nAgents x nAgents
+
+
+
+
+
+
+
+
+
+
+
+
+#######################################################################################        
+
         # Takes as input a tensor of shape
         #   nSamples x tSamples x 1 x nAgents
         # or of shape
         #   nSamples x 1 x nAgents
         # And returns the elementwise difference u_i - u_j of shape
         #   nSamples (x tSamples) x 1 x nAgents x nAgents
-        
+
         # Check dimensions
         assert len(u.shape) == 3 or len(u.shape) == 4
         # If it has shape 3, which means it's only a single time instant, then
