d68499488461371389008c7c102d90869030947f
Yan Zong <yan@yzong.com>
2024-03-28 19:58:55 +0800

    Modified to analyse the tanh function

--- untitled1.py
+++ untitled1.py
@@ -7,9 +7,7 @@ Created on Wed Mar 27 21:47:42 2024
 import os
 import pickle
 import numpy as np
-
-# with open("test", "rb") as fp:   # Unpickling
-# ...   b = pickle.load(fp)
+import matplotlib.pyplot as plt
 
 zeroTolerance = 1e-9 # values below this number are zero
 
@@ -22,67 +20,85 @@ for i in range(len(dataFolder)):
 # the following is for temperature use
 folderName = "TimeSync-050-20240320204410"
 saveDir = os.path.join(saveDirRoot, folderName)
-savedTanhDir = os.path.join(saveDir,'savedTanh')
-savedTanhDir = os.path.join(savedTanhDir,'1-100afterAct')
-#%%
+saveDir = os.path.join(saveDir,'savedTanh')
 
+#%%
 nAgents = 50
 duration = 10. # simulation duration 
 updateTime = 0.01 # clock update time
-
 tSamples = np.int64(duration/updateTime)
-readTime = np.array([1, 101, 201, 301, 401, 501, 601, 701, 801, 901])
+savingInstant = np.array([1, 101, 201, 301, 401, 501, 601, 701, 801, 901, 1001])
+preSavingInstant = np.array([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000])
 
-graphFile = np.load(saveFile + '-' + str(model.name) + '-nDAggers-' + str(nDAggers) + '.npz', allow_pickle=True) # the data file loaded from the example folder
-
+modelStructure = np.array(['2-64-2', '2-64-64-2', '2-64-64-2'])
+beforeActivationValue = np.array(['1-100-beforeAct', '101-200-beforeAct', '201-300-beforeAct', '301-400-beforeAct', '401-500-beforeAct', \
+                            '501-600-beforeAct', '601-700-beforeAct', '701-800-beforeAct', '801-900-beforeAct', '901-1000-beforeAct'])
+afterActivationValue = np.array(['1-100-afterAct', '101-200-afterAct', '201-300-afterAct', '301-400-afterAct', '401-500-afterAct', \
+                            '501-600-afterAct', '601-700-afterAct', '701-800-afterAct', '801-900-afterAct', '901-1000-afterAct'])
+    
+# for element in modelStructure:
+savedTanhDir = os.path.join(saveDir, '2-64-2')    
+graphFile = np.load(os.path.join(savedTanhDir, 'graph.npz'), allow_pickle=True)    
 graph = graphFile['graph']
 
+assert len(beforeActivationValue) == len(afterActivationValue)
+    
 laplacianMatrix = np.zeros((graph.shape[0], tSamples, nAgents, nAgents), dtype = np.float64)
 eigenValues = np.zeros((graph.shape[0], tSamples, nAgents), dtype = np.float64)
 eigenVectors = np.zeros((graph.shape[0], tSamples, nAgents, nAgents), dtype = np.float64)
-beforeActValues = np.zeros((graph.shape[0], tSamples, beforeAct[0][0].shape[1], nAgents), dtype = np.float64) # values before the activation function
-afterActValues = np.zeros((graph.shape[0], tSamples, afterAct[0][0].shape[1], nAgents), dtype = np.float64) # values after the activation function
+beforeActValues = np.zeros((graph.shape[0], tSamples, 64, nAgents), dtype = np.float64) # values before the activation function
+afterActValues = np.zeros((graph.shape[0], tSamples, 64, nAgents), dtype = np.float64) # values after the activation function
 
-i = 0
-
+index = 0
 for t in range(1, tSamples):
+    
+    if t == savingInstant[index]:
+        savedBeforeTanhDir = os.path.join(savedTanhDir, beforeActivationValue[0])
+        savedAfterTanhDir = os.path.join(savedTanhDir, afterActivationValue[0])
         
-    if t == readTime[i]:
-        del beforeActivation, afterActivation 
+        if 'beforeActivation' in globals():
+            del beforeActivation
+        if 'afterActivation' in globals():
+            del afterActivation                
         
-        with open(savedTanhDir, "rb") as fp:   # Unpickling
-            afterActivation = pickle.load(fp)
-            
-        with open(savedTanhDir, "rb") as fp:   # Unpickling
+        with open(savedBeforeTanhDir, "rb") as fp:   # Unpickling
             beforeActivation = pickle.load(fp)    
-            
-        i = i + 1    
-
-    for t in range(1, tSamples-1):
-        thisBeforeAct = beforeActivation[t][0] # values before the activation function
-        thisAfterAct = afterActivation[t][0] # values after the activation function    
         
-        thisGraph = graph[:,t,:,:]
-        thisGraph[thisGraph > zeroTolerance] = 1. # reset the normalised adjacency matrix to the normal graph matrix
-        thisAdjacencyMatrix = thisGraph
-        thisDegreeMatrix = np.sum(thisAdjacencyMatrix, axis=2)
+        with open(savedAfterTanhDir, "rb") as fp:   # Unpickling
+            afterActivation = pickle.load(fp)
         
-        for i in range(thisDegreeMatrix.shape[0]): # in the experiment instant dimension                    
-            laplacianMatrix[i, t, :, :] = np.diag(thisDegreeMatrix[i, :]) - thisAdjacencyMatrix[i, :, :]  # Non-Normalized laplacian matrix
-            # The column eigenvectors[:, i] is the normalized eigenvector corresponding to the eigenvalue eigenvalues[i]
-            eigenValues[i, t, :], eigenVectors[i, t, :, :] = np.linalg.eigh(laplacianMatrix[i, t, :, :])
+        index = index + 1
+    
+    thisBeforeAct = beforeActivation[(t-1) - preSavingInstant[index-1]][0] # values before the activation function
+    thisAfterAct = afterActivation[(t-1) - preSavingInstant[index-1]][0] # values after the activation function    
+        
+    thisGraph = graph[:,t,:,:]
+    thisGraph[thisGraph > zeroTolerance] = 1. # reset the normalised adjacency matrix to the normal graph matrix
+    thisAdjacencyMatrix = thisGraph
+    thisDegreeMatrix = np.sum(thisAdjacencyMatrix, axis=2)
 
-            assert thisBeforeAct.shape[1] == thisAfterAct.shape[1]
-            for j in range(thisBeforeAct.shape[1]): # in the feature dimension                    
-                beforeActValues[i, t, j, :] = np.matmul(np.transpose(eigenVectors[i, t, :, :]), np.float64(thisBeforeAct[i, j, :])) # values before the activation function
-                afterActValues[i, t, j, :] = np.matmul(np.transpose(eigenVectors[i, t, :, :]), np.float64(thisAfterAct[i, j, :])) # values after the activation function
+    for i in range(thisDegreeMatrix.shape[0]): # in the experiment instant dimension                    
+        laplacianMatrix[i, t, :, :] = np.diag(thisDegreeMatrix[i, :]) - thisAdjacencyMatrix[i, :, :]  # Non-Normalized laplacian matrix
+        # The column eigenvectors[:, i] is the normalized eigenvector corresponding to the eigenvalue eigenvalues[i]
+        eigenValues[i, t, :], eigenVectors[i, t, :, :] = np.linalg.eigh(laplacianMatrix[i, t, :, :])
 
-    for i in range(0, thisBeforeAct.shape[1]):
-        plt.figure()
-        plt.rcParams["figure.figsize"] = (6.4,4.8)
-        for t in range(0, tSamples-1):
-            plt.vlines(eigenValues[0, t, :], 0, beforeActValues[0, t, i, :], color='#D3D3D3', alpha=0.4)
-            plt.scatter(eigenValues[0, t, :], beforeActValues[0, t, i, :], color='#D3D3D3', alpha=0.4)
-            
-            plt.vlines(eigenValues[0, t, :], 0, afterActValues[0, t, i, :], color='#7BC8F6', alpha=0.4)
-            plt.scatter(eigenValues[0, t, :], afterActValues[0, t, i, :], color='#7BC8F6', alpha=0.4)
+        assert thisBeforeAct.shape[1] == thisAfterAct.shape[1]
+        for j in range(thisBeforeAct.shape[1]): # in the feature dimension                    
+            beforeActValues[i, t, j, :] = np.matmul(np.transpose(eigenVectors[i, t, :, :]), np.float64(thisBeforeAct[i, j, :])) # values before the activation function
+            afterActValues[i, t, j, :] = np.matmul(np.transpose(eigenVectors[i, t, :, :]), np.float64(thisAfterAct[i, j, :])) # values after the activation function
+
+#%%
+t = 500    
+cnt = 10                            
+for i in range(cnt+0, thisBeforeAct.shape[1]):
+    plt.figure()
+    plt.rcParams["figure.figsize"] = (6.4,4.8)
+    plt.vlines(eigenValues[0, t, :], 0, beforeActValues[0, t, i, :], color='#D3D3D3', alpha=0.4)
+    plt.scatter(eigenValues[0, t, :], beforeActValues[0, t, i, :], color='#D3D3D3', alpha=0.4)
+    
+    plt.vlines(eigenValues[0, t, :], 0, afterActValues[0, t, i, :], color='#7BC8F6', alpha=0.4)
+    plt.scatter(eigenValues[0, t, :], afterActValues[0, t, i, :], color='#7BC8F6', alpha=0.4)
+    
+    if i == cnt+9:
+        break
+        
